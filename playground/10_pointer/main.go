e6dd3d1adad319764cbdfb724c5a2a807c575234
TuanPham <tuan.pham@ad24.cz>
2022-02-27 15:17:30 +0100

    pointer comments update

--- playground/10_pointer/main.go
+++ playground/10_pointer/main.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 	a := 5
-	b := &a
+	b := &a //Set a pointer to A !!adress
 
 	fmt.Println(a, b)     // it will print 5 + memory address of a
 	fmt.Printf("%T\n", a) // a is int
 	fmt.Printf("%T\n", b) //b is *int * = pointer
 
 	// * to read val from address
-	fmt.Println(*b) //it will POINT cuz it's pointer to the value
-	fmt.Println(b) //print adress
+	fmt.Println(*b) //it will read the value of the pointer
+	fmt.Println(b)  //print adress that points to.
 
 	fmt.Println()
 }
